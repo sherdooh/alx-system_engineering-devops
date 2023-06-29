c60185b81186fc93f6f9b71bf4fac83b69c521c7
sherdooh <sherkiplah@gmail.com>
2023-06-28 23:46:02 -0700

    create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#creating a file using puppet
+
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
+
